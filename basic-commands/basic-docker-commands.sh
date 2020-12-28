3c4bbf11855c9169dfad91058941d230346bc67f
Chadwin Crouch <chadwinc@sahomeloans.com>
2020-12-28 11:11:21 +0200

    docker code add

--- basic-commands/basic-docker-commands.sh
+++ basic-commands/basic-docker-commands.sh
@@ -31,3 +31,34 @@ EXPOSE 80
 CMD ["node", "app.js"]
 EOF
 
+#create example node.js app
+cat > app.js <<EOF
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 80;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+        res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log('Server running at http://%s:%s/', hostname, port);
+});
+
+process.on('SIGINT', function() {
+    console.log('Caught interrupt signal and will exit');
+    process.exit();
+});
+EOF
+
+#build docker app in same directory as dockerfile
+docker build -t node-app:0.1 .
+
+#run docker container based on the image built
+docker run -p 4000:80 --name my-app node-app:0.1
+
+#test your docker app
+curl http://localhost:4000
