dd328d8c686101a281c6b527ba801281103bce3c
Chadwin Crouch <chadwinc@sahomeloans.com>
2020-12-28 08:05:57 +0200

    added docker file code

--- basic-commands/basic-docker-commands.sh
+++ basic-commands/basic-docker-commands.sh
@@ -3,3 +3,31 @@ docker run hello-world
 
 #check docker version
 docker --version
+
+#display docker images pulled
+docker images
+
+#view running docker containers
+docker ps
+
+#view all docker containers
+docker ps -a
+
+#create a docker file
+cat > Dockerfile <<EOF
+# Use an official Node runtime as the parent image
+FROM node:6
+
+# Set the working directory in the container to /app
+WORKDIR /app
+
+# Copy the current directory contents into the container at /app
+ADD . /app
+
+# Make the container's port 80 available to the outside world
+EXPOSE 80
+
+# Run app.js using node when the container launches
+CMD ["node", "app.js"]
+EOF
+
